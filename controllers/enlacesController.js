8628292829b291ad878310bf43c9d496c9547ac1
rodrigoSolano <rodrigosolano865@gmail.com>
2021-12-28 22:19:45 -0600

    Definir un password y un limite de descargas a los enlaces

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,21 +1,34 @@
 const shortid = require('shortid');
+const bcrypt = require('bcrypt');
 const Enlace = require('../models/Enlace');
 
 exports.nuevoEnlace = async (req, res, next) => {
   // Revisar si hay errores
 
-  // Si el usuario esta autenticado
-
   // Crear el enlace
   // eslint-disable-next-line camelcase
-  const { nombre_original, password } = req.body;
+  const { nombre_original } = req.body;
   const enlace = new Enlace();
   enlace.url = shortid.generate();
   enlace.nombre = shortid.generate();
   // eslint-disable-next-line camelcase
   enlace.nombre_original = nombre_original;
-  enlace.password = password;
-  console.log(enlace);
+
+  // Si el usuario esta autenticado
+  if (req.usuario) {
+    const { password, descargas } = req.body;
+
+    if (descargas) {
+      enlace.descargas = descargas;
+    }
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      enlace.password = await bcrypt.hash(password, salt);
+    }
+
+    enlace.autor = req.usuario.id;
+  }
+
   try {
     await enlace.save();
     res.json({
