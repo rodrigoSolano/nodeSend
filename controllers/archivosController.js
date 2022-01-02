6a30cfeb3f1a73ed6b5ccece8db8c4a230a03173
rodrigoSolano <rodrigosolano865@gmail.com>
2022-01-01 18:57:00 -0600

    Bug al momento de guardar archivos
    * Los archivos se guardan con dos puntos antes de la extension
    * El servidor no encuentra el archivo

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -14,7 +14,7 @@ exports.subirArchivo = async (req, res, next) => {
       },
       filename: (_req, file, cb) => {
         const extension = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
-        cb(null, `${shortid.generate()}.${extension}`);
+        cb(null, `${shortid.generate()}${extension}`);
       },
     }),
   };
